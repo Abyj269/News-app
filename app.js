9b635d72f9c7220392808dee9ae6f3c49007e572
Abyj269 <abyj269@gmail.com>
2021-11-04 15:22:24 +0530

    Basic sentry integration

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const express = require('express')
 const app = express();
-const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 const Router = require('./routes/authroute')
 const postRouter = require('./routes/postroute')
 const mongooseConnection = require('./connection/mongodb');
-const { mongo } = require('mongoose');
-const { json } = require('express');
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+Sentry.init({
+  dsn:process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All controllers should live here
+app.get("/", function rootHandler(req, res) {
+  res.end("Hello world!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 mongooseConnection()
 app.get('/',(req,res)=>{
